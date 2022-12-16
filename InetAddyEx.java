7863fb8fef64eda94485cb802a368a8b34d6ff2a
Michael Corbishley <79810775+michaelc143@users.noreply.github.com>
2022-12-15 18:40:26 -0600

    File that gets the users ip address and displays it as output

--- InetAddyEx.java
+++ InetAddyEx.java
@@ -3,8 +3,10 @@ public class InetAddyEx {
     public static void main(String[]args) throws UnknownHostException {
         String host = "google.com";
         try {
+            //gets address and stores as local host for google.com
             InetAddress localHost = InetAddress.getLocalHost();
             System.out.println("Local Host: " + localHost);
+            //gets address and mac address and displays both to screen
             InetAddress[] iAddresses = InetAddress.getAllByName(host);
             for(InetAddress ipAddy : iAddresses) {
                 System.out.println(ipAddy.toString());
